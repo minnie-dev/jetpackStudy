fded69beed010b0da77d7371ee2b06c9557081ad
minnie-dev <jung.dev98@gmail.com>
2022-01-21 15:24:53 +0900

    220121 databinding 실습

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.jetpackpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.jetpackpractice.databinding.ActivityMainBinding
+import com.example.jetpackpractice.databinding.DataBindingActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.dataBindingBtn.setOnClickListener {
+            val intent = Intent(this,DataBindingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
